bb5bc02a4e1e82edbbd3cca26cd16ea0a944d050
Jory Schmidt <joryschmidt1988@gmail.com>
2017-06-03 22:25:07 +0000

    create user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,19 @@
+var User = require('../models/User.model');
+
+exports.register = function(req, res) {
+  var newUser = new User();
+  newUser.username = req.body.username;
+  newUser.email = req.body.email;
+  newUser.password = req.body.password;
+  
+  newUser.save(function(err, user) {
+    if (err) {
+      console.log(err);
+      res.send("There was an error registering user");
+    }
+    else {
+      console.log(user);
+      res.send("You've signed up successfully");
+    }
+  });
+};
