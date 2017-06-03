d9729a59fbce74afab0f02952389cadc5dfb3117
Jory Schmidt <joryschmidt1988@gmail.com>
2017-06-03 22:08:29 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var bluebird = require('bluebird');
+
+var db = 'mongodb://' + process.env.IP + '/ssq';
+mongoose.Promise = bluebird;
+mongoose.connect(db);
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+
+var port = process.env.PORT;
+app.listen(port, function() {
+  console.log('App listening on port', port);
+});
