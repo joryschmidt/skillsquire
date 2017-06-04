5c9676b4ad3413cb194d4138ef5d8d678ef1cfaf
Jory Schmidt <joryschmidt1988@gmail.com>
2017-06-04 02:47:16 +0000

    add starter bootstrap template; likely to change

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 var user_controller = require('../controllers/user.controller');
 
+router.get('/', function(req, res) {
+  res.render('../views/newdex');
+});
 router.post('/', user_controller.register);
 
 module.exports = router;
