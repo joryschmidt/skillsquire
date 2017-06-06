df63781226d8c8cc1b941ed6fa8d00ba8ef4da1d
Jory Schmidt <joryschmidt1988@gmail.com>
2017-06-06 03:27:20 +0000

    home controller initial setup

--- views/app.js
+++ views/app.js
@@ -1,9 +1,16 @@
 (function() {
   var app = angular.module('ssq', ['ngRoute']);
   app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {
-      templateUrl: 'templates/home.html'
-    });
+    $routeProvider
+      .when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'templates/signup.html',
+        controller: 'signupCtrl'
+      })
+      .otherwise({ redirectTo: '/signup' });
   }]);
 
 })();
