afdabaeca8fed107f927c0a1ad3df7d787bcc373
Jory Schmidt <joryschmidt1988@gmail.com>
2017-07-03 19:35:41 +0000

    add rerouting on submit to admin functions

--- views/admin_views/controllers/create.admin.controller.js
+++ views/admin_views/controllers/create.admin.controller.js
@@ -1,11 +1,12 @@
 angular.module('admin')
 
-.controller('createAdminCtrl', ['$http', '$scope', function($http, $scope) {
+.controller('createAdminCtrl', ['$http', '$scope', '$location', function($http, $scope, $location) {
   $scope.admin = {
     isAdmin: true
   };
   
   $scope.submit = function() {
     $http({method: 'POST', url: '/admin', data: $scope.admin });
+    $location.path('/');
   };
 }]);
