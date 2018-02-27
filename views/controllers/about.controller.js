4ec53f5506f8bde9e8aa3602c65349a3b252b498
Jory Schmidt <joryschmidt1988@gmail.com>
2018-02-27 18:19:13 +0000

    work on about page

--- views/controllers/about.controller.js
+++ views/controllers/about.controller.js
@@ -1,5 +1,13 @@
 angular.module('ssq')
 
 .controller('aboutCtrl', ['$http', '$scope', function($http, $scope) {
-  
+  $scope.rsc = {
+    name: 'Resource Name',
+    className: 'resourcename',
+    link: '#!/about',
+    description: "This is a resource description",
+    long_description: 'This is a longer description of the resource',
+    color: 'aqua'
+  }; 
+
 }]);
