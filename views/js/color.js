8451aa4b0b51710d97e545e7c0445adfde42d023
Jory Schmidt <joryschmidt1988@gmail.com>
2018-07-24 01:14:17 +0000

    give the main heading some fancy, fancy colors

---
+++ views/js/color.js
@@ -0,0 +1,21 @@
+function getColor() {
+  function rNum() {
+    return Math.round(Math.random() * 255);
+  }
+  
+  var str = "rgb(";
+  for (var i=0; i<3; i++) {
+    if (i == 2) {
+      str = str + rNum() + ')';
+    }
+    else str = str + rNum() + ', ';
+  }
+  
+  return str;
+}
+
+setTimeout(function() {
+  $('.heading-letter').each(function(index, element) {
+    this.style.color = getColor();
+  });
+}, 1000);
